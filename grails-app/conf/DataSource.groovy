960c76c4ec00af798000b32ed0254adde293b069
Andrey Carmisini <ac3@cin.ufpe.br>
2015-08-06 11:45:42 -0300

    init

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -5,6 +5,8 @@ dataSource {
     username = "sa"
     password = ""
 }
+
+
 hibernate {
     cache.use_second_level_cache = true
     cache.use_query_cache = false
@@ -28,7 +30,7 @@ environments {
             url = "jdbc:h2:mem:testDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
         }
     }
-    production {
+    /*production {
         dataSource {
             dbCreate = "update"
             url = "jdbc:h2:prodDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
@@ -52,6 +54,19 @@ environments {
                jdbcInterceptors = "ConnectionState"
                defaultTransactionIsolation = java.sql.Connection.TRANSACTION_READ_COMMITTED
             }
+        }
+    }*/
+    production {
+        dataSource {
+            dbCreate = "update"
+            driverClassName = "org.postgresql.Driver"
+            dialect = org.hibernate.dialect.PostgreSQLDialect
+
+            uri = new URI(System.env.DATABASE_URL?:"postgres://test:test@localhost/test")
+
+            url = "jdbc:postgresql://" + uri.host + ":" + uri.port + uri.path
+            username = uri.userInfo.split(":")[0]
+            password = uri.userInfo.split(":")[1]
         }
     }
 }
